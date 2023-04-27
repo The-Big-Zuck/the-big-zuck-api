ddc0b1c9f2a12ae02666a96b7e5076f1acd528af
Caleb-Geiger-Schoolsin <caleb.geiger@schoolsin.com>
2023-04-27 17:16:25 -0400

    added post new item

--- the.big.zuck.Api/Controllers/CatalogController.cs
+++ the.big.zuck.Api/Controllers/CatalogController.cs
@@ -27,5 +27,11 @@ namespace the.big.zuck.Api.Controllers
 
             return Ok(item);
         }
+
+        [HttpPost]
+        public IActionResult Post(Item item)
+        {
+            return Created("/catalog/42", item);
+        }
     }
 }
