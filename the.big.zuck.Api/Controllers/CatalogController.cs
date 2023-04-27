d64758b816984c01f25e3299eaa89dc84ac08503
Caleb-Geiger-Schoolsin <caleb.geiger@schoolsin.com>
2023-04-27 17:11:35 -0400

    added get item by id

--- the.big.zuck.Api/Controllers/CatalogController.cs
+++ the.big.zuck.Api/Controllers/CatalogController.cs
@@ -15,8 +15,17 @@ namespace the.big.zuck.Api.Controllers
                 new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m),
                 new Item("Shorts", "Ohio State shorts.", "Nike", 44.99m)
             };
-            
+
             return Ok(items);
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetItem(int id)
+        {
+            var item = new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m);
+            item.Id = id;
+
+            return Ok(item);
         }
     }
 }
